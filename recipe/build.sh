d3c24f7e12ff94b077353e917182feb51e3074c3
Andrea Manica <am315@cam.ac.uk>
2026-07-15 16:48:59 +0100

    test intall all

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,8 @@
 
 set -euxo pipefail
 
-
 # Locate extracted source
 cd "${SRC_DIR}"
-
 
 # Build bundled HTSlib
 cd htslib
@@ -17,17 +15,12 @@ make \
 
 cd ../angsd
 
-
 # Build ANGSD against bundled HTSlib
-#
-# This is the upstream-recommended mechanism:
-# make HTSSRC=../htslib
 
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include -I${SRC_DIR}/htslib"
 export CFLAGS="${CFLAGS} -I${PREFIX}/include -I${SRC_DIR}/htslib -Xpreprocessor -fopenmp"
 export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${SRC_DIR}/htslib -Xpreprocessor -fopenmp"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -lomp"
-
 
 make \
     HTSSRC=../htslib \
@@ -38,20 +31,8 @@ make \
     CXXFLAGS="${CXXFLAGS}" \
     LDFLAGS="${LDFLAGS}"
 
-
-mkdir -p "${PREFIX}/bin"
-
-install -m 755 angsd "${PREFIX}/bin/"
-
-
-# Install auxiliary programs
-
-for exe in \
-    misc/realSFS \
-    misc/thetaStat \
-    misc/NGSadmix
-do
-    if [ -f "${exe}" ]; then
-        install -m 755 "${exe}" "${PREFIX}/bin/"
-    fi
-done
+# Install everything defined by upstream
+make \
+    HTSSRC=../htslib \
+    bindir="${PREFIX}/bin" \
+    install-all
